83d5e2eb0979428567ddc1df9c4316bc891c47b6
thisAtlas <magnus.kartvedt.p@gmail.com>
2017-01-19 12:27:35 +0100

    BreakOut Version 1
    The game works, lives and score is added, win- and failurestate.

--- game.js
+++ game.js
@@ -1,47 +1,81 @@
-//var c = document.getElementById('myCanvas');
-//var ctx = c.getContext('2d');
-
-//Funktion der tegner 3 objekter. Step #1.
-/*function init() {
-	var c = document.getElementById('myCanvas');
-	var ctx = c.getContext('2d');
-	
-	ctx.beginPath();
-	ctx.rect(20, 40, 50, 50);
-	ctx.rect(300,20,50,50);
-	ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-	ctx.stroke();
-	ctx.closePath();
-
-	ctx.beginPath();
-	ctx.arc(240, 160, 20, 0, Math.PI*2,false);
-	ctx.fillStyle='green';
-	ctx.fill();
-	ctx.closePath();
-	
-	ctx.beginPath();
-	ctx.arc(50, 50, 20, 0, Math.PI*2,false);
-	ctx.fillStyle='green';
-	ctx.fill();
-	ctx.closePath();
-}*/
+//Javascript for Breakout spil
+//De fleste variabler bliver defineret uden værdier globalt, og værdierne gives så inde i funktioner.
+//Først: Canvas-elementet i HTML
 var canvas;
 var ctx;
-
+//Variabler for bolden og bevælgelse af bolden
 var x;
 var y;
+var ddx;
 var dx;
 var dy;
 var ballRadius = 10;
+//Variabler for spillets 'paddle'.
+var paddleHeight;
+var paddleWidth;
+var paddleX;
+//Variabler for kontrollering af spillets 'paddle'.
+var pmx;
+var rightPressed;
+var leftPressed;
+//Variabler for 'bricks', altså 'målene' i spillet.
+var brickRowCount;
+var brickColumnCount;
+var brickWidth;
+var brickHeight;
+var brickPadding;
+var brickOffsetTop;
+var brickOffsetLeft;
 
+var bricks;
+
+//Score-variabel
+var score = 0;
+//Liv-variabel
+var lives = 3;
+
+//init() funktionen loader onload, så alting herinde vil loade samtidig med siden.
 function init() {
 	canvas = document.getElementById("myCanvas");
 	ctx = canvas.getContext("2d");
 	
+	rightPressed = false;
+	leftPressed = false;
+	
+	touchCtrl();
+	kbCtrl();
+	
 	x = canvas.width/2;
 	y = canvas.height-30;
-	dx = 20;
-	dy = 20;
+	ddx = 5;
+	dx = Math.random()*(ddx+ddx)-ddx;
+	dy = -4;
+	if(dx < 0.5 && dx > -0.5) {
+		document.location.reload();
+	}
+	
+	paddleHeight = 10;
+	paddleWidth = 75;
+	paddleX = (canvas.width-paddleWidth)/2;
+	
+	brickRowCount = 4;
+	brickColumnCount = 6;
+	brickWidth = 64;
+	brickHeight = 15;
+	brickPadding = 8;
+	brickOffsetTop = 30;
+	brickOffsetLeft = 24;
+	
+	//Brick-array skabes her.
+	bricks = [];
+	for(c=0; c<brickColumnCount; c++) {
+		bricks[c] = [];
+		for(r=0; r<brickRowCount; r++) {
+			bricks[c][r] = { x: 0, y: 0, status: 1 };
+		}
+	}
+	
+	draw();
 }
 //Tegner bolden
 function drawBall() {
@@ -53,42 +87,158 @@ function drawBall() {
 }
 //Tegner paddle 
 function drawPaddle() {
-	canvas = document.getElementById("myCanvas");
-	ctx = canvas.getContext("2d");
-	
-	var paddleHeight = 10;
-	var paddleWidth = 75;
-	var paddleX = (canvas.width-paddleWidth)/2;
-	
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight-5, paddleWidth, paddleHeight);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
-//Tegner på canvas on repeat. Tjekker for collisions.
+//Tegner bricks
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			if(bricks[c][r].status == 1) {
+				var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+				var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+				
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
+		}
+	}
+}
+//Function for at tjekke om bolden overlapper en brick.
+function collisionDetection() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+			if(b.status == 1) {
+				if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+					dy = -dy;
+					b.status = 0;
+					score++;
+					if(score == brickColumnCount*brickRowCount) {
+						alert("You win!");
+						document.location.reload();
+					}
+				}
+			}
+        }
+    }
+}
+//Tegner og beregner spillerens score.
+function drawScore() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.fillText("Score: " + score, 8, 20)
+}
+//Tegner og beregner spillerens liv.
+function drawLives() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.fillText("Lives: " + lives, canvas.width-65, 20)
+}
+
+//Keyboard controls
+function kbCtrl() {
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
+}
+function keyDownHandler(e) {
+	if(e.keyCode ==39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
+//Touch controls
+function touchCtrl() {
+	document.addEventListener("touchstart", pMoveStart, false);
+	document.addEventListener("touchend", pMoveEnd, false);
+}
+function pMoveStart() {
+	pmx = event.touches[0].pageX;
+	if(pmx > canvas.width/2) {
+		rightPressed = true;
+	}else if(pmx < canvas.width/2) {
+		leftPressed = true;
+	}
+}
+function pMoveEnd() {
+	if(pmx > canvas.width/2){
+		rightPressed = false;
+	}
+	else if(pmx < canvas.width/2){
+		leftPressed = false;
+	}
+}
+//Sletter og tegner på canvas on repeat. 
+//Tjekker for collisions.
 function draw() {
-	canvas = document.getElementById("myCanvas");
-	ctx = canvas.getContext("2d");
-	
+	//Første linje 'clearer' canvas-elementet, så der ikke er nogle tegninger gemt fra forrige loop.
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBricks();
 	drawBall();
 	drawPaddle();
-	
-	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-		dy = -dy;
-	}
+	drawScore();
+	drawLives();
+	//Tjekker for collison
+	collisionDetection();
+	//Vender bolden hvis den rører ved en af siderne på canvas-elementet (x-axis collision).
 	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
 		dx = -dx;
 	}
-	
+	//Vender bolden hvis den rører toppen af canvas-elementet (+y.axis collision).
+	if(y + dy < ballRadius) {
+		dy = -dy;
+	}
+	//Hvis bolden rammer 'paddle' boksen vender den vertikalt
+	else if(y + dy > canvas.height-ballRadius-3) {
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		}
+		//Hvis ikke får man en 'Game over' besked, og siden reloader så man kan spille igen.
+		else {
+			lives--;
+			if(!lives) {
+				alert("Game Over. Try again?");
+				document.location.reload();
+			}
+			else {
+				x = canvas.width/2;
+                y = canvas.height-30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width-paddleWidth)/2;
+			}
+		}
+	}
+	//Flytter 'paddle' når input er givet.
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+		paddleX += 5;
+	}
+	else if(leftPressed && paddleX > 0) {
+		paddleX -= 5;
+	}
+	//Flyter boldens placering til næste loop - virker som 'bevægelse'.
 	x += dx;
     y += dy;
-}
-//Controls
-function controlHandler() {
 	
+	//Gentager draw() funktionen når 
+	requestAnimationFrame(draw);
 }
-
-//Kører draw() functionen i et x millisekunder interval, hvor x er talværdienm bag
-setInterval(draw, 10);
